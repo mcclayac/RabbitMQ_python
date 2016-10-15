f2a743b1187499bc79a6b0045b508f92c457243b
Anthony McClay <anthony.mcclay@nielsen.com>
2016-10-15 09:00:57 -0400

    Initial CHeck in

---
+++ Routing/emit_log.py
@@ -0,0 +1,26 @@
+__author__ = 'anthonymcclay'
+__project__ = 'RabbitMQ'
+__date__ = '10/15/16'
+__revision__ = '$'
+__revision_date__ = '$'
+
+
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='direct_logs',
+                         type='direct')
+
+severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+message = ' '.join(sys.argv[2:]) or 'Hello World!'
+channel.basic_publish(exchange='direct_logs',
+                      routing_key=severity,
+                      body=message)
+print(" [x] Sent %r:%r" % (severity, message))
+connection.close()
+
